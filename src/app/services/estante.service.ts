6868f0f4628a934c2c3838388201c7b205a3f4a3
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-21 00:59:51 -0300

    services realizados

--- src/app/services/estante.service.ts
+++ src/app/services/estante.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Estante } from '../models/estante';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstanteService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/estante'
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.http.get(this.url)
+  }
+  
+  add(estante: Estante): Observable<any> {
+    return this.http.post(this.url + '/addEstante', estante)
+  }
+  
+  edit(estante: Estante): Observable<any> {
+    return this.http.post(this.url + '/' + estante.id + '/update', estante)
+  }
+  
+  delete(id: number): Observable<any> {
+    return this.http.post(this.url + '/' + id + '/delete', null)
+  }
+  
 }
--- src/app/services/libro.service.ts
+++ src/app/services/libro.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Libro } from '../models/libro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibroService {
+  
+  private url = 'http://localhost:8080/libro'
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.http.get(this.url)
+  }
+  
+  add(libro: Libro): Observable<any> {
+    return this.http.post(this.url + '/addLibro', libro)
+  }
+  
+  edit(libro: Libro): Observable<any> {
+    return this.http.post(this.url + '/' + libro.id + '/update', libro)
+  }
+  
+  delete(id: number): Observable<any> {
+    return this.http.post(this.url + '/' + id + '/delete', null)
+  }
+
 }
