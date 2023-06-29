a75f687b916821ce1739e8e583a8c7fc41935628
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-29 19:11:37 -0300

    titleservice oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AplicacionesInteractivasFrontend';
+export class AppComponent implements OnInit{  
+  
+  constructor(private titleService: Title) { }
+
+ngOnInit() {
+  this.titleService.setTitle('Inicio');
+}
+
   navbarCollapsed = true;
 }
