9a3b44952b5abd2669897ebb629f740f1c130626
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-06-29 19:15:31 -0300

    atributo estante libro

--- src/app/models/libro.ts
+++ src/app/models/libro.ts
@@ -1,7 +1,10 @@
+import { Estante } from "./estante"
+
 export class Libro {
     id: number
     titulo: string
     descripcion: string
     autor: string
     editorial: string
+    estante: Estante; 
 }
